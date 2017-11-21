003c0b4c5c907850915e493e4fdae3faafc19f2c
eu2004 <utma.emilian@gmail.com>
2017-11-21 21:52:32 +0100

    Updated script to execute the client jar

--- dist/run.cmd
+++ dist/run.cmd
@@ -1,4 +1,4 @@
 set JAVA_HOME=jre1.8.0_144
 
 rem %JAVA_HOME%\bin\java -classpath ".;classes" ro.eu.passwallet.client.flexui.ApplicationLauncher
-%JAVA_HOME%\bin\java -jar passwallet.jar
+%JAVA_HOME%\bin\java -jar passwallet-fx-client.jar
