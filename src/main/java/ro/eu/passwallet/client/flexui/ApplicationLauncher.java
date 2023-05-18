f3f99682af32a8c5298bdae0858020a077a10d10
Emilian <utma.emilian@gmail.com>
2023-05-18 10:21:32 +0200

    clearClipboardHook on exit app

--- src/main/java/ro/eu/passwallet/client/flexui/ApplicationLauncher.java
+++ src/main/java/ro/eu/passwallet/client/flexui/ApplicationLauncher.java
@@ -4,10 +4,11 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
 import javafx.stage.Stage;
 
 public class ApplicationLauncher extends Application {
-
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("open_passwallet_ui.fxml"));
@@ -16,8 +17,20 @@ public class ApplicationLauncher extends Application {
         primaryStage.show();
     }
 
+    @Override
+    public void stop() {
+        clearClipboardHook();
+    }
 
     public static void main(String[] args) {
         launch(args);
+    }
+
+    private static void clearClipboardHook() {
+        final Clipboard clipboard = Clipboard.getSystemClipboard();
+        final ClipboardContent content = new ClipboardContent();
+        content.putString(" ");
+        clipboard.setContent(content);
+        System.out.println("Clipboard cleared");
     }
 }
